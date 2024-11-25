8dbe1e90d96807a654a51c4f73086d98a1e67595
gabriel.ganassin <gabrielganassin.gg@gmail.com>
2024-11-25 20:37:13 -0300

    adicionando enunciado

--- Mundo 1/ex014(convertTemp).py
+++ Mundo 1/ex014(convertTemp).py
@@ -1,2 +1,4 @@
+# Exercício Python 14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
 temp = float(input('Qual a temperatura de hoje em ºC? '))
 print(f'Esses {temp}ºC correspondem a {(temp*(9/5))+32}ºF')
