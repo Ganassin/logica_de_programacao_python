7b7ae223160143226c78aa4b0ecec586cb62c413
gabriel.ganassin <gabrielganassin.gg@gmail.com>
2025-01-02 15:10:09 -0300

    edicao ex112

--- Mundo 3/modulos/dado_112.py
+++ Mundo 3/modulos/dado_112.py
@@ -2,9 +2,9 @@ def leiadinheiro(msg):
     valido = False
     
     while not valido:
-        preco = str(input(f'\n{msg}')).replace(',','.')
+        preco = str(input(f'\n{msg}')).replace(',','.').strip()
         
-        if preco.isalpha():
+        if preco.isalpha() or preco == '':
             print(f'\033[0;31mERRO! {preco} é um preço inválido!\033[m')
         else: 
             valido = True
